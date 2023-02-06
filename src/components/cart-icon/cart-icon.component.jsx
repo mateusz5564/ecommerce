168a6e1302c1b6f19d92f6620365341b85c0f79f
mateusz5564 <mateusz5564@gmail.com>
2023-02-06 16:18:33 +0100

    convert CartIcon styles to styled components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,15 @@
-import { ReactComponent as Icon } from "../../assets/shopping-bag.svg";
 import { useCartContext } from "../../context/cart.context";
-import "./cart-icon.styles.scss";
+import "./cart-icon.styles.jsx";
+import { CartIconContainer, ItemCount, StyledIcon } from "./cart-icon.styles.jsx";
 
 const CartIcon = () => {
   const { cartCount, setIsCartOpen } = useCartContext();
 
   return (
-    <div className="cart-icon-container" onClick={() => setIsCartOpen(prevState => !prevState)}>
-      <Icon className="icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={() => setIsCartOpen(prevState => !prevState)}>
+      <StyledIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
