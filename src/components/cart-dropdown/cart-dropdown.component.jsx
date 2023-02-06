ce61560170e5b80653d59ad149213ead9fa3478c
mateusz5564 <mateusz5564@gmail.com>
2023-02-06 14:01:04 +0100

    convert CartDropdown styles to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,22 +2,24 @@ import { useNavigate } from "react-router-dom";
 import { useCartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import "./cart-dropdown.styles.scss";
+import { CartDropdownContainer, CartItems, EmptyMessage } from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const navigate = useNavigate();
   const { cartItems } = useCartContext();
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.length < 1 && <p className="empty-message">Your cart is empty</p>}
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length < 1 && (
+          <EmptyMessage className="empty-message">Your cart is empty</EmptyMessage>
+        )}
         {cartItems.map(item => (
           <CartItem key={item.id} product={item} />
         ))}
-      </div>
+      </CartItems>
       <Button onClick={() => navigate("checkout")}>Go To Checkout</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
